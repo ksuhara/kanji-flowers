e9d5afd1844c97de6688f41c97238f686135643f
kenta suhara <ponta.blockchain@gmail.com>
2021-03-04 10:12:56 +0900

    test

--- src/CustomStyle.js
+++ src/CustomStyle.js
@@ -57,6 +57,7 @@ const CustomStyle = ({
         // https://docs.opensea.io/docs/metadata-standards
         // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1155.md#erc-1155-metadata-uri-json-schema
 
+        
         attributes: [
           {
             display_type: 'number',
